c6ca52a4acd33d7c5c07a65b8b6deeb2780103fe
stiffold <stary.jaroslav@gmail.com>
2015-05-02 21:34:11 +0200

    Podpora pro záznam hry :pig:  prasak pattern
    Je to zatím dost zabugované a naprasáka, ale náznak tam je

--- SnakeDeathmatch/MainWindow.xaml.cs
+++ SnakeDeathmatch/MainWindow.xaml.cs
@@ -10,24 +10,39 @@ using System.Collections.Generic;
 using SnakeDeathmatch.Game;
 using SnakeDeathmatch.Tests;
 using SnakeDeathmatch.Interface;
+using SnakeDeathmatch.Views;
+using System.IO;
+using System.Net;
 
 namespace SnakeDeathmatch
 {
     public partial class MainWindow : Window
     {
-        public const int PlaygroundSizeInDots = 100;
+        public int PlaygroundSizeInDots = 100;
         public const int PlaygroundSizeInPixels = 600;
         public const int Speed = 60;
         public const int TestsSpeed = 5;
 
         private DispatcherTimer _timer = new DispatcherTimer();
+        private DispatcherTimer _replayTimer = new DispatcherTimer();
         private GameEngine _gameEngine;
         private bool _isShowingTests = false;
+        private bool _isShowingReply = false;
         private string _lastTestName;
         private Random _random = new Random();
 
         private int[,] _previousArray;
         private bool _shouldClearWindowAfterRestartGame;
+
+        EndGameDialog _dialog = new EndGameDialog();
+        OpenReplayDialog _opendialog = new OpenReplayDialog();
+        List<RecordLine> _records = new List<RecordLine>();
+        int _round = 1;
+        int _replayStep = 1;
+
+        private string _ftpServerIP = "ftp.hostuju.cz/";
+        private string _ftpUserName = "snake.hostuju.cz";
+        private string _ftpPassword = "123snake123";
 
         public MainWindow()
         {
@@ -44,7 +59,15 @@ namespace SnakeDeathmatch
             _timer.Tick += UpdateGameSurround;
             _timer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / Speed);
 
-            Restart();
+            _dialog.Title = Title;
+            _dialog.btnYes.Focus();
+            _dialog.btnYes.Click += (s, n) => { Restart(); };
+            _dialog.btnSave.Click += (s, n) => { Save(); };
+            _dialog.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+
+            _opendialog.btnOpen.Click += (s, n) => { Open(); };
+
+            //Restart();
         }
 
         private Direction GetRandomDirection()
@@ -68,9 +91,95 @@ namespace SnakeDeathmatch
             return players;
         }
 
+        private void Save()
+        {
+            if (String.IsNullOrEmpty(_dialog.txtFileName.Text)) 
+            {
+                MessageBox.Show("Název souboru musí být vyplněn!");
+                return;
+            }
+
+            //generate csv
+            string path = Environment.CurrentDirectory;
+            string filename = String.Format("{0}\\{1:MMddyyyy}{2}.csv", path, DateTime.Now, _dialog.txtFileName.Text);
+
+            if (!File.Exists(filename))
+            {
+                File.Create(filename).Close();
+            }
+
+            using (System.IO.TextWriter writer = File.CreateText(filename))
+            {
+                writer.WriteLine(PlaygroundSizeInDots);
+                foreach (var r in _records.OrderBy(x=>x.Round))
+                {
+                    writer.WriteLine(String.Format("{0};{1};{2};{3};{4}", r.Round, r.Name, r.Color, r.X, r.Y));
+                }
+            }
+       
+            //save to FTP
+
+
+            FileInfo objFile = new FileInfo(filename);
+            FtpWebRequest objFTPRequest;
+
+            // Create FtpWebRequest object 
+            objFTPRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + _ftpServerIP + "/" + objFile.Name));
+
+            // Set Credintials
+            objFTPRequest.Credentials = new NetworkCredential(_ftpUserName, _ftpPassword);
+
+            // By default KeepAlive is true, where the control connection is 
+            // not closed after a command is executed.
+            objFTPRequest.KeepAlive = false;
+
+            // Set the data transfer type.
+            objFTPRequest.UseBinary = true;
+
+            // Set content length
+            objFTPRequest.ContentLength = objFile.Length;
+
+            // Set request method
+            objFTPRequest.Method = WebRequestMethods.Ftp.UploadFile;
+
+            // Set buffer size
+            int intBufferLength = 16 * 1024;
+            byte[] objBuffer = new byte[intBufferLength];
+
+            // Opens a file to read
+            FileStream objFileStream = objFile.OpenRead();
+
+            try
+            {
+                // Get Stream of the file
+                Stream objStream = objFTPRequest.GetRequestStream();
+
+                int len = 0;
+
+                while ((len = objFileStream.Read(objBuffer, 0, intBufferLength)) != 0)
+                {
+                    // Write file Content 
+                    objStream.Write(objBuffer, 0, len);
+
+                }
+
+                objStream.Close();
+                objFileStream.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            MessageBox.Show(filename +" uploadován :-)");
+            _dialog.txtFileName.Text = null;
+        }
+
         private void Restart()
         {
             _timer.Stop();
+            _round = 1;
+            _records.Clear();
 
             if (_isShowingTests)
             {
@@ -91,6 +200,14 @@ namespace SnakeDeathmatch
             _shouldClearWindowAfterRestartGame = true;
             _timer.Start();
         }
+
+        private void Stop()
+        {
+            _timer.Stop();
+            _round = 1;
+            _records.Clear();
+        }
+
 
         private GameEngine GetGameEngineForNextTest()
         {
@@ -121,6 +238,7 @@ namespace SnakeDeathmatch
 
         private void UpdateGameSurround(object sender, EventArgs e)
         {
+            
             if (_gameEngine == null)
             {
                 _canvas.Children.Clear();
@@ -130,15 +248,14 @@ namespace SnakeDeathmatch
             if (!_gameEngine.GameOver)
             {
                 RenderArray(_gameEngine.Move());
+                _round++;
             }
             else
             {
                 _timer.Stop();
-                if (MessageBox.Show(string.Format(_isShowingTests ? "{0}\r\nSpustit další test?" : "{0}\r\nSpustit další deathmatch?", _gameEngine.ScoreMessage()),
-                    Title, MessageBoxButton.YesNo, MessageBoxImage.None, MessageBoxResult.Yes) == MessageBoxResult.Yes)
-                {
-                    Restart();
-                }
+                _dialog.lblResult.Content = _gameEngine.ScoreMessage();
+                _dialog.lblRestart.Content = _isShowingTests ? "Spustit další test?" : "Spustit další deathmatch?";
+                _dialog.ShowDialog();
             }
         }
 
@@ -171,11 +288,39 @@ namespace SnakeDeathmatch
                 {
                     if (array[x, y] != 0 && IsPointInArrayChanged(array, x, y))
                     {
-                        AddRectangle(x, y, _gameEngine.GetColorForIdentificator(array[x, y]), dotSizeInPixels);
+                        PlayerId id = (PlayerId)(array[x, y]);
+                        var color = _gameEngine.GetColorForIdentificator((int)id);
+                        AddRectangle(x, y, color, dotSizeInPixels);
+                        _records.Add(new RecordLine(_round, x,y,color,id.ToString()));
                     }
                 }
             }
             _previousArray = (int[,])array.Clone();
+
+        }
+
+        private void RenderReplayArray(object sender, EventArgs e)
+        {
+            int dotSizeInPixels = PlaygroundSizeInPixels / PlaygroundSizeInDots;
+            if (_records.Any(x => x.Round == _replayStep))
+            {
+                foreach (var s in _records.Where(x => x.Round == _replayStep))
+                {
+                    AddRectangle(s.X, s.Y, s.Color, dotSizeInPixels);
+                }
+                _replayStep++;
+            }
+            else
+            {
+                _replayTimer.Stop();
+                var result = _records.Where(x=>x.Name != "-1").GroupBy(x => x.Name).OrderByDescending(s=>s.Max(l => l.Round)).Select(g => g.Key.ToString() +" "+ g.Max(y => y.Round).ToString());
+                string message = "";
+                foreach (var r in result)
+                {
+                    message += r + "\r\n";
+                }
+                MessageBox.Show(message);
+            }
 
         }
 
@@ -198,12 +343,104 @@ namespace SnakeDeathmatch
         private void _buttonRestart_Click(object sender, RoutedEventArgs e)
         {
             _isShowingTests = false;
+            _isShowingReply = false;
             Restart();
+        }
+
+        private void _buttonReplay_Click(object sender, RoutedEventArgs e)
+        {
+            _isShowingTests = false;
+            _isShowingReply = true;            
+            Replay();
+        }
+
+        private void _buttonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the object used to communicate with the server.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://"+_ftpServerIP);
+            request.Method = WebRequestMethods.Ftp.ListDirectory;
+
+            // This example assumes the FTP site uses anonymous logon.
+            request.Credentials = new NetworkCredential(_ftpUserName, _ftpPassword);
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            List<string> files = (reader.ReadToEnd()).Split(new string[] { "\r\n", "\n","\r" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                       
+            reader.Close();
+            response.Close();
+
+            _opendialog.lstFiles.Items.Clear();
+
+            foreach (var f in files.Where(x=>x.Contains(".csv")))
+            {
+                _opendialog.lstFiles.Items.Add(f);
+            }
+            _opendialog.Show();            
+        }
+
+        private void Replay()
+        {
+             StartReplay();
+        }
+
+        private void Open()
+        {
+             Stop();
+             LoadReplay();
+             StartReplay();
+        }
+
+        private void LoadReplay()
+        {
+            string fileName = _opendialog.lstFiles.SelectedItem.ToString();
+            List<string> records = new List<string>();
+            WebClient request = new WebClient();
+            string url = "ftp://"+_ftpServerIP + fileName;
+            request.Credentials = new NetworkCredential(_ftpUserName, _ftpPassword);
+            request.Proxy = null;
+
+            try
+            {
+                byte[] newFileData = request.DownloadData(url);
+                string fileString = System.Text.Encoding.UTF8.GetString(newFileData);
+                records = (fileString).Split(new string[] { "\r\n", "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            }            
+            catch (WebException e)
+            {
+                // Do something such as log error, but this is based on OP's original code
+                // so for now we do nothing.
+            }
+
+            if (records.Any())
+            {
+                int firstRow = Int32.Parse(records[0]);
+                PlaygroundSizeInDots = firstRow;
+
+                _records = records.Skip(1)
+                           .Select(x => x.Split(';'))
+                           .Select(n => new RecordLine(Int32.Parse(n[0]), Int32.Parse(n[3]), Int32.Parse(n[4]), (Color)ColorConverter.ConvertFromString(n[2]), n[1])).ToList();
+            }
+
+            Replay();
+
+        }
+
+        private void StartReplay()
+        {
+            _replayStep = 1;
+            _canvas.Children.Clear();
+            _replayTimer.Tick += RenderReplayArray;
+            _timer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / Speed);
+            _replayTimer.Start();
         }
 
         private void _buttonTests_Click(object sender, RoutedEventArgs e)
         {
             _isShowingTests = true;
+            _isShowingReply = false;
             Restart();
         }
     }
---
+++ SnakeDeathmatch/Views/OpenReplayDialog.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SnakeDeathmatch.Views
+{
+    /// <summary>
+    /// Interaction logic for OpenReplay.xaml
+    /// </summary>
+    public partial class OpenReplayDialog : Window
+    {
+        public OpenReplayDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
